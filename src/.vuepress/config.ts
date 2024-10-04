79141451452f479b3b30d441e704ec417df1e0e2
Yaoabcde <1964877312@qq.com>
2024-10-04 17:19:22 +0800

    加入base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/yaoweb/",
 
   lang: "zh-CN",
   title: "博客演示",
