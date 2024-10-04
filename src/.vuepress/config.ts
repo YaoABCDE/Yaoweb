30d88e2f984511c1f95b9a44403cef53be2f0b67
Yaoabcde <1964877312@qq.com>
2024-10-04 17:33:03 +0800

    更改大小写

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/yaocc/",
+  base: "/Yaoweb/",
 
   lang: "zh-CN",
   title: "博客演示",
